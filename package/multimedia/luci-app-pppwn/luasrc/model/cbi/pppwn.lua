f5ad0803968dae3072d8a57c5f19730d880e16f5
syb999 <13062678135@163.com>
2026-01-02 20:58:02 +0800

    luci-app-pppwn: compat luci-24.10

--- package/multimedia/luci-app-pppwn/luasrc/model/cbi/pppwn.lua
+++ package/multimedia/luci-app-pppwn/luasrc/model/cbi/pppwn.lua
@@ -23,8 +23,9 @@ end
 fwver=s:option(Value, "fwver", translate("PlayStation 4 System Version"))
 fwver.default = "1100"
 fwver.rmempty = false
-fwver:value("700", translate("7.00"))
 fwver:value("900", translate("9.00"))
+fwver:value("960", translate("9.60"))
+fwver:value("1000", translate("10.00"))
 fwver:value("1100", translate("11.00"))
 fwver.description = translate("1100 means Ver 11.00 etc.")
 
